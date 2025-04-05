d3b29b4c41e0d238e9379e941147bd7fc64acd24
davide131297 <chiffi.davide@gmx.de>
2025-04-05 18:57:40 +0200

    erstellung GetRate Funktion

--- funtions/get-rate.ts
+++ funtions/get-rate.ts
@@ -1,15 +1,10 @@
 import { supabase } from '@/initSupabase';
 import { Ratings } from '@/types/db-types';
 
-export default async function GiveRate({ song_id, moderator_id, user_id, rating }: Ratings) {
+export default async function GetRate({ song_id, moderator_id, user_id, rating }: Ratings) {
   return supabase
     .from('ratings')
-    .insert({
-      song_id,
-      moderator_id,
-      user_id,
-      rating,
-    })
+    .select('*')
     .then(({ data, error }) => {
       if (error) {
         throw new Error(error.message);
---
+++ types/db-types.ts
@@ -0,0 +1,6 @@
+export type Ratings = {
+  song_id: number | null;
+  moderator_id: number | null;
+  user_id: number;
+  rating: number;
+};
